78327e7020980c058f6a7a8894dfddb964b11534
dana_mn <dana.mn@gmail.com>
2016-09-15 13:34:52 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var main = function() {
+  $(".nav-main").find("a").hover( 
+    function() {
+    $(this).addClass("hover");
+  }, function() {
+    $(this).removeClass("hover");
+  })
+ 
+  $(".scroll").click(function(event){     
+      event.preventDefault();
+   $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+}
+$(document).ready(main)
