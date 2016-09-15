2a4615a57312e608e97bf65076fb2b427ddc518b
dana_mn <dana.mn@gmail.com>
2016-09-15 16:16:34 -0400

    add second toggled text

--- script.js
+++ script.js
@@ -14,9 +14,8 @@ var main = function() {
 
   $(".services-button").click(function(){
 
+      $(this).parent().find(".service-info-toggled").toggleClass('hide-div');
 
-      console.log("click");
-      $(this).parents().find(".service-info-toggled").toggleClass('hide-div');
 
      if ($(this).find('.btn-text').html() == "Click for details"){
         $(this).find('.btn-text').html('Click to hide details');
@@ -24,8 +23,16 @@ var main = function() {
           $(this).find('.btn-text').html('Click for details');
       }
    
-      
-    });
+/*   var parentSection = $(this).parents().find('.section-services');
+      if (parentSection.css('height', '150vh')){
+        console.log('ok');
+        parentSection.css('height', '250vh');
+      } else {
+        parentSection.css('height', '150vh');
+      }
+
+*/
+    })
 
 }
 
